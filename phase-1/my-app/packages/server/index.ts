adaa51b1d9db6ba7100b83d297b3eeb071241eee
amirethio <amiraliyimer@gmail.com>
2025-09-05 21:17:15 +0300

    manage hugging face api

--- phase-1/my-app/packages/server/index.ts
+++ phase-1/my-app/packages/server/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import express from "express";
+import type{ Request , Response }  from "express";
+import dotenv from 'dotenv'
+
+dotenv.config({})
+
+const app =  express()
+const port  = process.env.PORT
+
+
+
+app.get("/" , (req: Request,res:Response)=>{
+    res.send("hello world!")
+})
+
+app.listen(port ,()=>{
+    console.log(`server is running on http://localhost:${port}`);
+    
+})
